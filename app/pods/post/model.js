dc68c2b522c6bf831fe72f6bea620f78444088d9
Ivan Lavriv <lavriv92@gmail.com>
2015-10-15 22:42:18 +0300

    Added application

---
+++ app/pods/post/model.js
@@ -0,0 +1,16 @@
+import DS from 'ember-data';
+
+let Post = DS.Model.extend({
+  title: DS.attr(),
+  content: DS.attr()
+});
+
+
+Post.reopenClass({
+  FIXTURES: [
+    { id: 1, title: 'Title1', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+    { id: 2, title: 'Title2' , content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+  ]
+});
+
+export default Post;
